0faddd2db7116062eaf7a0a3011c53d170a291d2
nikhil-lather <nikhillather02687@gmail.com>
2026-08-04 23:59:23 +0530

    Describe what you changed

---
+++ 06-objects.js
@@ -0,0 +1,19 @@
+1. Creating Objects
+
+There are multiple ways to create objects.
+
+Method 1 (Most Common) - Object Literal
+const person = {
+    name: "Nikhil",
+    age: 22,
+    city: "Delhi"
+};
+
+console.log(person);
+Method 2 - new Object()
+const person = new Object();
+
+person.name = "Nikhil";
+person.age = 22;
+
+console.log(person);
